4ec7bbc9cf547f6c28a0338e7806a82efb48cacd
Tor Hovland <tor.hovland@tweag.io>
2022-07-06 15:05:15 +0200

    Working Wasm.

--- README.md
+++ README.md
@@ -42,6 +42,7 @@ wasm-pack test --firefox --headless
 Following the cargo2nix instructions, we generate a `Cargo.nix` file by running:
 
 ```
+cd variants/cargo2nix-workspace
 nix run github:cargo2nix/cargo2nix
 git add Cargo.nix
 ```
@@ -52,9 +53,19 @@ Then we can simply build and run our native app like this:
 nix run
 ```
 
-However, when it comes to building the Wasm code, cargo2nix is unable to help, and it is a known issue:
+However, the Wasm code does not get output by cargo2nix, and it is a known issue:
 
 https://github.com/cargo2nix/cargo2nix/issues/203
+
+After some debugging (with Alexei and Yorick) it turned out that the solution was simple, and fixed in https://github.com/cargo2nix/cargo2nix/pull/283. Until it gets merged, we run against `github:torhovland/cargo2nix/wasm`.
+
+So, in the end, we build our Wasm code like this:
+
+```
+cd variants/cargo2nix-workspace
+nix build
+ls result/lib/wasm.wasm 
+```
 
 ## crate2nix
 
--- variants/cargo2nix-workspace/flake.nix
+++ variants/cargo2nix-workspace/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    cargo2nix.url = "github:torhovland/cargo2nix/wasm";
     flake-utils.follows = "cargo2nix/flake-utils";
     nixpkgs.follows = "cargo2nix/nixpkgs";
   };
@@ -8,20 +8,53 @@
   outputs = inputs: with inputs;
     flake-utils.lib.eachDefaultSystem (system:
       let
+        wasmTarget = "wasm32-unknown-unknown";
+
         pkgs = import nixpkgs {
           inherit system;
+          overlays = [cargo2nix.overlays.default];
+        };
+        
+        wasmPkgs = import nixpkgs {
+          inherit system;
+          crossSystem = {
+            config = "wasm32-unknown-wasi-unknown";
+            system = "wasm32-wasi";
+            useLLVM = true;
+          };
           overlays = [cargo2nix.overlays.default];
         };
 
         rustPkgs = pkgs.rustBuilder.makePackageSet {
           rustVersion = "1.61.0";
           packageFun = import ./Cargo.nix;
+          packageOverrides = pkgs: pkgs.rustBuilder.overrides.all ++ [
+            (pkgs.rustBuilder.rustLib.makeOverride {
+              overrideAttrs = _: {
+                __contentAddressed = true;
+              };
+            })
+          ];
+        };
+        
+        rustPkgsWasm = wasmPkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.61.0";
+          packageFun = import ./Cargo.nix;
+          target = "wasm32-unknown-unknown";
+          packageOverrides = pkgs: pkgs.rustBuilder.overrides.all ++ [
+            (pkgs.rustBuilder.rustLib.makeOverride {
+              overrideAttrs = _: {
+                __contentAddressed = true;
+              };
+            })
+          ];
         };
 
       in rec {
+        inherit rustPkgs rustPkgsWasm;
         packages = {
           app = (rustPkgs.workspace.app {}).bin;
-          wasm = (rustPkgs.workspace.wasm {}).lib;
+          wasm = (rustPkgsWasm.workspace.wasm {}).out;
           default = packages.app;
         };
       }
